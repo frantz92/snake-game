7af03b689e64c12e5486c28158ba7cb17dfa47ec
Patryk Frankowski <patryk.frankowski@o2.pl>
2020-06-17 13:50:24 +0200

    Init project, add game board, add dots represanting the snake

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Snake from './Snake';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    snakeDots: [
+      [0, 0],
+      [2, 0],
+      [4, 0],
+    ]
+  }
+
+  render() {
+    return (
+      <div className='game-area'>
+        <Snake snakeDots={this.state.snakeDots} />
+      </div>
+    );
+  }
 }
 
 export default App;
